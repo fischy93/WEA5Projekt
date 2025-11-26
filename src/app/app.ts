700704122e48711d4bfc8eaba7d9aa8df19c06de
Patrick Fischlhammer <fischy008@yahoo.de>
2025-11-26 17:24:01 +0100

    Contact daten laden funktioniert

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-
 import { authConfig } from './auth.config';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { AuthenticationService } from './services/authentication';
 import { CustomerSyncService } from './services/customer-sync.service';
 import { environment } from './environments/environments';
 import { HttpClient } from '@angular/common/http';
+import { SessionService } from './services/session.service';
 
 @Component({
   selector: 'app-root',
@@ -21,28 +21,28 @@ export class App {
     private oauthService: OAuthService,
     public auth: AuthenticationService,
     private sync: CustomerSyncService,
-    private http: HttpClient
+    private http: HttpClient,
+    private session: SessionService
   ) {
     // Wichtig: OIDC konfigurieren
     this.oauthService.configure(authConfig);
 
-    // Hier warten wir auf fertigen Login
+    //  auf fertigen Login warten
     this.oauthService.loadDiscoveryDocumentAndTryLogin().then(() => {
       this.afterLogin();
     });
   }
 
-  // Diese Methode läuft erst WENN der Login wirklich abgeschlossen ist
   private afterLogin() {
     console.log("TryLogin abgeschlossen");
 
     if (this.auth.isLoggedIn()) {
       console.log("Login abgeschlossen → Tokens vorhanden");
 
-      // Claims anzeigen
-      console.log("ID TOKEN:", this.oauthService.getIdToken());
-      console.log("ACCESS TOKEN:", this.oauthService.getAccessToken());
-      console.log("CLAIMS:", this.oauthService.getIdentityClaims());
+      // // Claims anzeigen
+      // console.log("ID TOKEN:", this.oauthService.getIdToken());
+      // console.log("ACCESS TOKEN:", this.oauthService.getAccessToken());
+      // console.log("CLAIMS:", this.oauthService.getIdentityClaims());
 
       // Customer aus Keycloak bauen
       const customer = this.sync.buildCustomerFromKeycloak();
@@ -57,13 +57,37 @@ export class App {
             console.log("Customer existiert NICHT → wird gespeichert...");
 
             this.http.post(environment.api + '/customer', customer)
-              .subscribe(res => console.log("Customer gespeichert:", res));
+              .subscribe(res => {
+                console.log("Customer gespeichert:", res);
+
+                // *** CustomerID nach dem Erstellen laden ***
+                this.loadCustomerId(customer?.UserName);
+              });
+
           } else {
             console.log("Customer existiert bereits → kein Insert notwendig.");
+
+            // *** CustomerID laden ***
+            this.loadCustomerId(customer?.UserName);
           }
+
         });
     } else {
       console.log("Nicht eingeloggt – Login-Flow wird später getriggert.");
     }
   }
+
+
+  private loadCustomerId(username: string) {
+    this.http.get<any>(environment.api + '/customer/' + username)
+      .subscribe(c => {
+        if (c) {
+          this.session.customerId = c.id;
+          console.log("CustomerID gesetzt:", this.session.customerId);
+        }
+      });
+  }
+
+
+
 }

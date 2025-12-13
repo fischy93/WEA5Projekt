02268ac6593867e0a0115b0c5f99a189133f3d91
Patrick Fischlhammer <fischy008@yahoo.de>
2025-12-13 23:09:40 +0100

    Addresse Name und Country hinzugefügt

--- src/app/shared/address.ts
+++ src/app/shared/address.ts
@@ -3,11 +3,13 @@ export class Address {
         public street?: string,
         public houseNumber?: string,
         public zip?: number,
-        public city?: string
+        public city?: string,
+        public name?: string,
+        public country?: string
     ) { }
 
     public addressIsComplete(): boolean {
-        return !!this.street && !!this.houseNumber && !!this.zip && !!this.city;
+        return !!this.street && !!this.houseNumber && !!this.zip && !!this.city && !!this.name && !!this.country;
     }
 
 }
